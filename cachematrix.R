a078bddcdf6755ba861fbd0917b169b5a7b68138
Luis Caro <luis.caro@finac.com>
2015-08-21 15:42:20 -0500

    hw3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## creates the special matrix
+makeCacheMatrix<-function(x=matrix()){
+	inv<-NULL
+	set<-function(y){
+		x<<-y
+		inv<<-NULL
+		}
+	get<-function() x
+	setsolve<-function(solve) inv <<-solve
+	getsolve<-function() inv
+	list(set=set,get=get,setsolve=setsolve,getsolve=getsolve)
+	}
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'. Calculates it only if it is not cached yet
+cacheSolve<-function(x, ...){
+	inv<-x$getsolve()
+	if(!is.null(inv)){
+		message("setting inverse data")
+		return(inv)
+	}
+	data<-x$get()
+	inv<-solve(data, ...)
+	x$setsolve(inv)
+	inv
+	}
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+#sm<-makeCacheMatrix(matrix(rnorm(25), 5, 5))
+#cacheSolve(sm)
+
+#makeCacheMatrix <- function(x = matrix()) {
+#
+#}
+#cacheSolve <- function(x, ...) {
+#        ## Return a matrix that is the inverse of 'x'
+#}
+
+
+
